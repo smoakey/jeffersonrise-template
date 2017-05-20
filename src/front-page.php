7d36eddd9f2ffc8ad6888373298e545cf87c5292
Chris Smoak <csmoak@schoorunner.org>
2017-05-19 23:18:20 -0600

    Our model section

--- src/front-page.php
+++ src/front-page.php
@@ -10,13 +10,41 @@
         </section>
 
         <section class="call-to-action">
-            <a href="#" class="button action">Apply Now &raquo;</a>
-            <h3 class="title">Interested Families</h3>
-            <p class="no-margin">Are you interested in enrolling your child at Jefferson RISE?  Does your child live in Jefferson Parish? Is your child ENTERING the 6th, 7th, or 8th grade this fall (2017)?</p>
+            <div class="container">
+                <div class="eight columns">
+                    <h3 class="title">Interested Families</h3>
+                    <p class="no-margin">Are you interested in enrolling your child at Jefferson RISE?  Does your child live in Jefferson Parish? Is your child ENTERING the 6th, 7th, or 8th grade this fall (2017)?</p>
+                </div>
+                <div class="four columns">
+                    <a href="#" class="button button-large button-block button-solid-red">Apply Now &raquo;</a>
+                </div>
+            </div>
         </section>
 
         <section class="our-model">
-
+            <div class="container text-center">
+                <h3 class="title text-white">Our Model</h3>
+                <h6 class="subtitle text-light-gray text-center margin-bottom-fourty">
+                    Preparing students for four-year colleges and professional careers.
+                </h6>
+                <div class="row">
+                    <div class="four columns text-white text-center">
+                        <i class="fa fa-graduation-cap fa-4x text-red margin-bottom-twenty" aria-hidden="true"></i>
+                        <h5 class="no-margin">Academics</h5>
+                        <p class="text-light-gray">With targeted turtoring, independent reading, and extra time on reading and math, Jefferson RISE students will be ready for college.</p>
+                    </div>
+                    <div class="four columns text-white text-center">
+                        <i class="fa fa-user-circle-o fa-4x text-red margin-bottom-twenty" aria-hidden="true"></i>
+                        <h5 class="no-margin">Character</h5>
+                        <p class="text-light-gray">Respect<br />Integrity<br />Self-Discipline<br />Excellence</p>
+                    </div>
+                    <div class="four columns text-white text-center">
+                        <i class="fa fa-calendar-check-o fa-4x text-red margin-bottom-twenty" aria-hidden="true"></i>
+                        <h5 class="no-margin">Student Life</h5>
+                        <p class="text-light-gray">Longer school days and year, enrinchment, skill blocks, targeting tutoring, and community circle.</p>
+                    </div>
+                </div>
+            </div>
         </section>
 
         <section class="">
