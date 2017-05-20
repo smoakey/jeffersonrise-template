98ff69b4116eca01c117f97138641b5fde61dac5
Chris Smoak <csmoak@schoorunner.org>
2017-05-20 01:17:18 -0600

    move our model page custom fields

--- src/front-page.php
+++ src/front-page.php
@@ -12,11 +12,13 @@
         <section class="call-to-action">
             <div class="container">
                 <div class="eight columns">
-                    <h3 class="title">Interested Families</h3>
-                    <p class="no-margin">Are you interested in enrolling your child at Jefferson RISE?  Does your child live in Jefferson Parish? Is your child ENTERING the 6th, 7th, or 8th grade this fall (2017)?</p>
+                    <h3 class="title"><?php the_field('call_to_action_title'); ?></h3>
+                    <p class="no-margin"><?php the_field('call_to_action_description'); ?></p>
                 </div>
                 <div class="four columns">
-                    <a href="#" class="button button-large button-block button-solid-red">Apply Now &raquo;</a>
+                    <a href="<?php the_field('call_to_action_button_link'); ?>" class="button button-large button-block button-solid-red">
+                        <?php the_field('call_to_action_button_text'); ?> &raquo;
+                    </a>
                 </div>
             </div>
         </section>
@@ -25,24 +27,17 @@
             <div class="container text-center">
                 <h3 class="title text-white">Our Model</h3>
                 <h6 class="subtitle text-light-gray text-center margin-bottom-fourty">
-                    Preparing students for four-year colleges and professional careers.
+                    <?php the_field('our_model_subtitle'); ?>
                 </h6>
                 <div class="row">
-                    <div class="four columns text-white text-center">
-                        <i class="fa fa-graduation-cap fa-4x text-red margin-bottom-twenty" aria-hidden="true"></i>
-                        <h5 class="no-margin">Academics</h5>
-                        <p class="text-light-gray">With targeted turtoring, independent reading, and extra time on reading and math, Jefferson RISE students will be ready for college.</p>
-                    </div>
-                    <div class="four columns text-white text-center">
-                        <i class="fa fa-user-circle-o fa-4x text-red margin-bottom-twenty" aria-hidden="true"></i>
-                        <h5 class="no-margin">Character</h5>
-                        <p class="text-light-gray">Respect<br />Integrity<br />Self-Discipline<br />Excellence</p>
-                    </div>
-                    <div class="four columns text-white text-center">
-                        <i class="fa fa-calendar-check-o fa-4x text-red margin-bottom-twenty" aria-hidden="true"></i>
-                        <h5 class="no-margin">Student Life</h5>
-                        <p class="text-light-gray">Longer school days and year, enrinchment, skill blocks, targeting tutoring, and community circle.</p>
-                    </div>
+                    <?php foreach([1,2,3] as $item) : ?>
+                        <div class="four columns text-white text-center">
+                            <i class="fa fa-<?php the_field('our_model_item' . $item . '_icon'); ?> fa-4x text-red margin-bottom-twenty" aria-hidden="true"></i>
+                            <h5 class="no-margin"><?php the_field('our_model_item' . $item . '_name'); ?></h5>
+                            <p class="text-light-gray"><?php the_field('our_model_item' . $item . '_description'); ?></p>
+                            <a class="button button-white button-solid-white-hover" href="<?php the_field('our_model_item' . $item . '_link'); ?>">Learn More</a>
+                        </div>
+                    <?php endforeach; ?>
                 </div>
             </div>
         </section>
