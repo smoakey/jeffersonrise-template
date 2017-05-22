a2c1ebf56d725b8479ff619cf46bc484c097a8ef
Chris Smoak <csmoak@schoorunner.org>
2017-05-21 20:40:52 -0600

    Output details on student app custom post type

--- src/inc/post-types/student-app.php
+++ src/inc/post-types/student-app.php
@@ -1,5 +1,7 @@
 <?php
 add_action('init', 'create_student_app_post_type');
+add_filter('manage_student_app_posts_columns', 'add_student_app_post_column');
+add_action('manage_student_app_posts_custom_column', 'add_student_app_post_column_content', 10, 2);
 
 function create_student_app_post_type() {
     register_post_type('student_app', [
@@ -17,4 +19,25 @@ function create_student_app_post_type() {
         'show_in_menu' => 'portal',
         'supports' => ['title']
     ]);
+}
+
+function add_student_app_post_column($columns) {
+    unset($columns['date']);
+    $columns['icon'] = _('Icon');
+    $columns['link'] = _('Link');
+    return $columns;
+}
+
+function add_student_app_post_column_content($column, $post_id) {
+    switch($column)
+    {
+        case 'icon':
+            $image = get_field($column, $post_id);
+            echo '<img src="' . $image . '" width="50" />';
+            break;
+        case 'link':
+            $link = get_field($column, $post_id);
+            echo '<a target="_blank" href="' . $link . '">' . $link . '</a>';
+            break;
+    }
 }
