c71ecd93e0aa903706beabbbfe6253da33c86deb
Chris Smoak <csmoak@schoorunner.org>
2017-05-24 22:26:30 -0600

    Updated site title

--- src/header.php
+++ src/header.php
@@ -2,7 +2,8 @@
 <html <?php language_attributes(); ?>>
     <head>
         <meta charset="<?php bloginfo( 'charset' ); ?>">
-        <title><?php wp_title('|', true, 'right'); ?><?php bloginfo('name'); ?></title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title><?php wp_title('|', true, 'right'); ?><?php bloginfo('name'); ?>, <?php bloginfo('description'); ?></title>
         <?php wp_head(); ?>
     </head>
     <body <?php body_class(); ?>>
