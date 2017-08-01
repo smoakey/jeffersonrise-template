3cbaf1c109165c8a69999345b76e24d8f9ba3434
Chris Smoak <csmoak@schoorunner.org>
2017-07-31 20:31:22 -0600

    Updating update script to backup db before overwriting locally

--- update.sh
+++ update.sh
@@ -28,8 +28,17 @@ printf "Copying DB Dump to remote...."
 scp -q db_dump.sql mt:~/domains/jeffersonrise.christophersmoak.me/
 success
 
+printf "Backing up Remote DB..."
+today=`date '+%Y_%m_%d_%H_%M_%S'`
+ssh mt "cd ~/domains/jeffersonrise.christophersmoak.me/db_dumps; mysqldump --compatible=mysql4 -h internal-db.s87759.gridserver.com -u 1clk_wp_gHX23aD -psEFJ73Ek db87759_wp > \"db_bak_$today.sql\""
+success
+
 printf "Loading From Dump...."
 ssh mt "mysql -h internal-db.s87759.gridserver.com -u 1clk_wp_gHX23aD -psEFJ73Ek db87759_wp < ~/domains/jeffersonrise.christophersmoak.me/db_dump.sql"
+success
+
+printf "Removing db_dump...."
+ssh mt "rm ~/domains/jeffersonrise.christophersmoak.me/db_dump.sql"
 success
 
 printf "Removing Local Dump...."
